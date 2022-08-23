6ddfe769e1fc6fb7f72a2063bc9972c962f527b0
Ksenia Pilshchikova <ksushapi@gmail.com>
2022-08-23 20:08:31 +0200

    Update NavAuth.jsx

--- src/components/nav-bar/NavAuth.jsx
+++ src/components/nav-bar/NavAuth.jsx
@@ -1,13 +1,16 @@
+import { Box } from "components/Box";
 import { NavDropdown } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 export const NavAuth = () => {
     return (
-        <NavDropdown title="Log in to your account" id="nav-dropdown">
-            <NavDropdown.Item to="/login" as={NavLink}>Login</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item to="/register" as={NavLink}>Register</NavDropdown.Item>
-        </NavDropdown>
+        <Box as="div" ml="auto"> 
+            <NavDropdown title="Log in to your account" id="nav-dropdown">
+                <NavDropdown.Item to="/login" as={NavLink}>Login</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item to="/register" as={NavLink}>Register</NavDropdown.Item>
+            </NavDropdown>
+        </Box>
     )
 }
 
