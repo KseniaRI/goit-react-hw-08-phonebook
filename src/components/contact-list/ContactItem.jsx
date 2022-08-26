f0c56208b36e065ae5234719be114162f49e640d
Ksenia Pilshchikova <ksushapi@gmail.com>
2022-08-26 14:00:21 +0200

    Update ContactItem.jsx

--- src/components/contact-list/ContactItem.jsx
+++ src/components/contact-list/ContactItem.jsx
@@ -5,6 +5,7 @@ import { getIsLoading } from "redux/contacts/phonebook-selectors";
 import PropTypes from 'prop-types';
 import { ClipLoader } from "react-spinners"
 import { DeleteButton, ItemWrap, StyledListGroupItem} from "./ContactList.styled"
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 
 export const ContactItem = ({ id, name, number }) => {
@@ -12,22 +13,38 @@ export const ContactItem = ({ id, name, number }) => {
     const dispatch = useDispatch();
 
     const [selectedId, setSelectedId] = useState(null);
+
+    // function Trigger() {
+    //     const renderTooltip = (props) => (
+    //         <Tooltip id="button-tooltip" {...props}>
+    //             The contact will be deleted defenetly
+    //         </Tooltip>
+    //     );
+    // }
    
     return (
         <StyledListGroupItem as="li"
             action>
             <ItemWrap>{name}:</ItemWrap><ItemWrap>{number}</ItemWrap>
             <ItemWrap>
-            <DeleteButton type="button" onClick={() => {
-                setSelectedId(id);
-                dispatch(deleteContact(id));
-                }
-            }
-                disabled={isDeleting}>
-                {id === selectedId && <ClipLoader loading={isDeleting} size={10} />}  
-                Delete
-                </DeleteButton>
-                </ItemWrap>
+                <OverlayTrigger
+                    placement="right"
+                    delay={{ show: 250, hide: 400 }}
+                    overlay={   <Tooltip id="button-tooltip">
+                                    Are you sure? The contact will be deleted.
+                                </Tooltip>}
+                >
+                    <DeleteButton type="button"
+                        onClick={() => {
+                        setSelectedId(id);
+                        dispatch(deleteContact(id));
+                         }}
+                        disabled={isDeleting}>
+                        {id === selectedId && <ClipLoader loading={isDeleting} size={10} />}  
+                        Delete
+                    </DeleteButton>
+                </OverlayTrigger>
+            </ItemWrap>
         </StyledListGroupItem>
     )
 }
